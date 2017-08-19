d8a09e4e9bd6dad15f1b9fd72a3a40c3c38cd0c5
micaela berry <msberry9@gmail.com>
2017-08-19 13:58:25 -0400

    figuring it out

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -2,13 +2,76 @@ package bowling;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 
 public class BowlingTest {
+	
+	BowlingGame bowlingGame;
+	
+	@Before
+	public void setUp(){
+	bowlingGame = new BowlingGame();
+	}
+
+    @Test
+    public void shouldScoreGutterGame() {
+        rollMany(20, 0);
+
+        assertThat(bowlingGame.getScore(), is(0));
+    }
+
 
 	@Test
-	public void shouldReturnBowlingScore (){
-		BowlingGame bowlingGame = new BowlingGame();
-		assertEquals(0, bowlingGame.score());
-	}
+    public void shouldScoreAllOnes() {
+        rollMany(20, 1);
+
+        assertThat(bowlingGame.getScore(), is(20));
+    }
+
+    @Test
+    public void shouldScoreOneSpare() {
+        rollSpare();
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+
+        assertThat(bowlingGame.getScore(), is(16));
+    }
+    
+    @Test
+    public void shouldScoreOneStrike() {
+        rollStrike();
+        bowlingGame.roll(3);
+        bowlingGame.roll(4);
+        rollMany(16, 0);
+
+        assertThat(bowlingGame.getScore(), is(24));
+    }
+
+    @Test
+    public void shouldScorePerfectGame() {
+        rollMany(12, 10);
+
+        assertThat(bowlingGame.getScore(), is(300));
+    }
+
+    private void rollMany(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++)
+            bowlingGame.roll(pins);
+    }
+
+    private void rollSpare() {
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+    }
+
+    private void rollStrike() {
+        bowlingGame.roll(10);
+    }
+
+
 }
